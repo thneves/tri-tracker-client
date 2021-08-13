d1b312d50f72c9c35b32a0f2f0f88ae258619355
thneves <tsneves11@gmail.com>
2021-08-13 16:32:14 -0300

    Create logout function with api request

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import Login from './Login';
 import Register from './Register';
 
-const Home = () => (
-  <>
-    <h1>Tri Tracker</h1>
-    <Register />
-  </>
-);
+const Home = () => {
+  const checkLoginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', { withCredentials: true })
+      .then(response => {
+        console.log('logged in?', response.data);
+      }).catch(error => {
+        console.log('login error', error);
+      });
+  };
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, [checkLoginStatus]);
+
+  const handleLogout = () => {
+    axios.delete('http://localhost:3001/logout', { withCredentials: true })
+      .then(response => {
+        console.log(response);
+      }).catch(error => {
+        console.log('logout error', error);
+      });
+  };
+
+  return (
+    <>
+      <h1>Tri Tracker</h1>
+      <h2>Register Form</h2>
+      <Register />
+      <br />
+      <h2>Login Form</h2>
+      <Login />
+      <button type="button" onClick={() => handleLogout()}>Logout</button>
+    </>
+  );
+};
 
 export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    registrationErrors: '',
+  });
+
+  const history = useHistory();
+
+  const handleSubmit = e => {
+    axios.post('http://localhost:3001/sessions', {
+      user: {
+        email: user.email,
+        password: user.password,
+      },
+    },
+    { withCredentials: true }).then(response => {
+      console.log('login response', response);
+    }).catch(error => {
+      console.log('error', error);
+    });
+
+    e.preventDefault();
+    history.push('/dashboard');
+  };
+
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Your email" value={user.email} onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Passsword" value={user.password} onChange={handleChange} required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
