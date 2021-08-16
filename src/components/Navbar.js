d3bde6e37c319b262528273cbbdbb4f45d2bffc8
thneves <tsneves11@gmail.com>
2021-08-16 01:33:54 -0300

    Style navbar, add icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/components/Navbar.scss';
+import ProgressIcon from '../assets/images/progress.png';
+import DashIcon from '../assets/images/dashboard.png';
+import TracksIcon from '../assets/images/tracks.png';
+import MoreIcon from '../assets/images/more.png';
 
 const Navbar = () => (
   <>
     <div className="navbar">
-      <Link className="nav-item" to="/dashboard">Dashboard</Link>
-      <Link className="nav-item" to="/tracks">Track It</Link>
-      <Link className="nav-item" to="/progress">Progress</Link>
-      <Link className="nav-item" to="/more">More</Link>
+      <Link className="nav-item" to="/dashboard">
+        Dashboard
+        <img src={DashIcon} alt="tri icon" />
+      </Link>
+      <Link className="nav-item" to="/tracks">
+        Track It
+        <img src={TracksIcon} alt="tracks icon" />
+      </Link>
+      <Link className="nav-item" to="/progress">
+        Progress
+        <img src={ProgressIcon} alt="progress icon" />
+      </Link>
+      <Link className="nav-item" to="/more">
+        More
+        <img src={MoreIcon} alt="more icon" />
+      </Link>
     </div>
   </>
 );
