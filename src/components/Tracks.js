32073a43ebb6d31af09d54f3af6b71db15a635a0
thneves <tsneves11@gmail.com>
2021-08-16 06:37:11 -0300

    Create helper methods for formatting dates

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,21 +1,23 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { fetchAllTracks } from '../redux/thunk/thunkGet';
+import dateFormat from '../helpers/dates';
 import Navbar from './Navbar';
 import Logout from '../containers/Logout';
 
 const Tracks = () => {
+  const allTracks = useSelector(state => state.allTracks.tracks);
+  let printTracks;
+
+  if (allTracks.length > 0) {
+    printTracks = allTracks.map(track => <p key={track.id}>{dateFormat(track.day)}</p>);
+  }
+
+  console.log(allTracks);
+
   useEffect(() => {
     fetchAllTracks();
   }, [fetchAllTracks]);
-
-  const allTracks = useSelector(state => state.allTracks.tracks);
-  const printTracks = allTracks.map(track => (
-    <div key={track.id}>
-      <p>{track.sport}</p>
-      <p>{track.day}</p>
-    </div>
-  ));
 
   return (
     <>
---
+++ src/helpers/dates.js
@@ -0,0 +1,42 @@
+const dateFormat = day => {
+  const d = new Date(day);
+
+  const year = d.getFullYear();
+  const date = d.getDate();
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const monthIndex = d.getMonth();
+  const monthName = months[monthIndex];
+  const dayIndex = d.getDay();
+  const dayName = days[dayIndex];
+
+  const formatted = `${dayName}, ${date}, ${monthName}, ${year}`;
+
+  return formatted;
+};
+
+export default dateFormat;
