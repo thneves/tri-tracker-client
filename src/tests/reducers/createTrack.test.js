089adec91d83f155550b0f2e6d576024f850a276
thneves <tsneves11@gmail.com>
2021-08-20 10:24:01 -0300

    Create tests for redux actions

--- src/tests/reducers/createTrack.test.js
+++ src/tests/reducers/createTrack.test.js
@@ -1,4 +1,4 @@
-import { CREATE_TRACK_REQUEST, CREATE_TRACK_SUCCESS, CREATE_TRACK_FAILURE } from "../../redux/actions/actionTypes";
+import { CREATE_TRACK_REQUEST, CREATE_TRACK_SUCCESS, CREATE_TRACK_FAILURE } from '../../redux/actions/actionTypes';
 import createTrack, { initialState } from '../../redux/reducers/createTrack';
 
 test('should return initial state', () => {
@@ -6,37 +6,37 @@ test('should return initial state', () => {
     {
       loading: false,
       track: {},
-      error: ''
-    }
-  )
-})
+      error: '',
+    },
+  );
+});
 
 test('should handle create request', () => {
   expect(createTrack(initialState, {
-    type: CREATE_TRACK_REQUEST
+    type: CREATE_TRACK_REQUEST,
   })).toEqual({
     ...initialState,
     loading: true,
-  })
-})
+  });
+});
 
 test('should handle create success', () => {
   expect(createTrack(initialState, {
-    type: CREATE_TRACK_SUCCESS
+    type: CREATE_TRACK_SUCCESS,
   })).toEqual({
     ...initialState,
     loading: false,
     track: undefined,
-    error: ''
-  })
-})
+    error: '',
+  });
+});
 
 test('should handle create track failure', () => {
   expect(createTrack(initialState, {
-    type: CREATE_TRACK_FAILURE
+    type: CREATE_TRACK_FAILURE,
   })).toEqual({
     loading: false,
     track: {},
-    error: undefined
-  })
-})
+    error: undefined,
+  });
+});
